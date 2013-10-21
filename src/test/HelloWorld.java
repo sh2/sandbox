c432fd9efe3ef22d6735c4c7281ac85b22a3d293
Sadao Hiratsuka <sh2@pop01.odn.ne.jp>
2013-10-21 10:35:31 +0900

    Hello Worldを更新

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -3,6 +3,6 @@ package test;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("こんにちは、世界！");
-		System.out.println("さらにこんにちは、世界！");
+		// System.out.println("さらにこんにちは、世界！");
 	}
 }
