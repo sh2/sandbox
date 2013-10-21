df5ff069ebf5914b005e46617e2f6d6dc63c68c8
Sadao Hiratsuka <sh2@pop01.odn.ne.jp>
2013-10-21 10:00:36 +0900

    HelloWorldを編集

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("こんにちは、世界！");
 		System.out.println("さらにこんにちは、世界！");
+		System.out.println("もういちどこんにちは、世界！");
 	}
 }
